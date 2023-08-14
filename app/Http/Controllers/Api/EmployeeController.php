b0b18a8690351699fb63063097adfdd2e7b8ec4b
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-08-13 22:51:33 -0300

    adiciona anotações do swagger para method store de employee e swagger ao projeto

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -9,13 +9,30 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\EmployeeStoreFormRequest;
 use App\Http\Resources\Employee\EmployeeCollection;
 use App\Http\Resources\Employee\EmployeeResource;
-use App\Models\Address;
 use App\Models\Employee;
-use App\Models\PhoneNumber;
 use App\Services\AddressService;
 use App\Services\EmployeeService;
 use App\Services\PhoneNumberService;
 use Illuminate\Http\Request;
+
+/**
+ * @OA\Info(
+ *   title="API Desafio Laravel Code 2023.2",
+ *   version="1.5.0",
+ *   contact={
+ *     "email": "joao.pedreira@estudante.ufjf.br"
+ *   }
+ * )
+ * @OA\SecurityScheme(
+ *  type="http",
+ *  description="Acess token obtido na autenticação",
+ *  name="Authorization",
+ *  in="header",
+ *  scheme="bearer",
+ *  bearerFormat="JWT",
+ *  securityScheme="bearerToken"
+ * )
+ */
 
 class EmployeeController extends Controller
 {
@@ -37,7 +54,47 @@ class EmployeeController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\POST(
+     *  tags={"Employee"},
+     *  summary="Create a new employee",
+     *  description="This endpoint creates a new employee",
+     *  path="/api/employees",
+     *  @OA\RequestBody(
+     *      @OA\MediaType(
+     *          mediaType="application/x-www-form-urlencoded",
+     *          @OA\Schema(
+     *             required={"name","email","password","password_confirmation","birthdate","neighborhood","street","zip_code","number","country","ddd","phone_number"},
+     *             @OA\Property(property="name", type="string", example="Gabriel Nunes"),
+     *             @OA\Property(property="email", type="string", example="gabriel_nunes@example.org"),
+     *             @OA\Property(property="password", type="string", example="#sdasd$ssdaAA@"),
+     *             @OA\Property(property="password_confirmation", type="string", example="#sdasd$ssdaAA@"),
+     *             @OA\Property(property="birthdate", type="string", example="1995-08-15"),
+     *             @OA\Property(property="neighborhood", type="string", example="Centro"),
+     *             @OA\Property(property="street", type="string", example="Rua do passarinho"),
+     *             @OA\Property(property="zip_code", type="string", example="37894457"),
+     *             @OA\Property(property="number", type="string", example="247"),
+     *             @OA\Property(property="country", type="string", example="55"),
+     *             @OA\Property(property="ddd", type="string", example="47"),
+     *             @OA\Property(property="phone_number", type="string", example="77849874"),
+     *          )
+     *      ),
+     *  ),
+     *  @OA\Response(
+     *    response=200,
+     *    description="Employee created",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="Employee", type="string", example="['id': 21,'user_id': 32,'work_time': null]")
+     *    )
+     *  ),
+     *  @OA\Response(
+     *    response=422,
+     *    description="Incorrect fields",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="The email has already been taken"),
+     *       @OA\Property(property="errors", type="string", example="..."),
+     *    )
+     *  )
+     * )
      */
     public function store(EmployeeStoreFormRequest $request)
     {
